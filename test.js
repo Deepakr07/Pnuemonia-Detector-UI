90944a0b432268f225350e9e95e307d9868c2edd
deepak <deepakrajesh567@gmail.com>
2024-03-12 21:37:03 +0530

    added a image discarding option on hovering

--- test.js
+++ test.js
@@ -6,9 +6,14 @@ function previewImage() {
         var reader = new FileReader();
 
         reader.onload = function(e) {
-            uploadContainer.innerHTML = '<img src="' + e.target.result + '" class="img-fluid">';
+            uploadContainer.innerHTML = '<img src="' + e.target.result + '" class="img-fluid"><span class="remove-icon" onclick="removeImage()">X</span>';
         }
 
         reader.readAsDataURL(fileInput.files[0]);
     }
+}
+
+function removeImage() {
+    var uploadContainer = document.getElementById('uploadContainer');
+    uploadContainer.innerHTML = '<label for="upload" class="upload-icon"><i class="fa fa-upload"></i></label><input type="file" id="upload" style="display: none;" onchange="previewImage()"><p class="upload-text">Upload Image</p>';
 }
